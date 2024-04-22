3763e1bc87042d438cb0e74b8f6764fa84ff5289
RubensCarvalhoRocha <rocharubens4@gmail.com>
2024-04-22 20:28:46 -0300

    Creating mock data for the account information.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccountInfo } from './account-info';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'control-flow-if';
+  account: AccountInfo = {
+    name: 'Melisa Evan',
+    membershipStatus: 'gold',
+    validThru: '12/2022',
+    CVV: '123',
+  };
 }
